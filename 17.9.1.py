2277a23c72136a70d5bfc93ac4a9532aefb622e3
Alenalena52 <118267598+Alenalena52@users.noreply.github.com>
2023-02-02 14:07:10 +0300

    Update 17.9.1.py

--- 17.9.1.py
+++ 17.9.1.py
@@ -1,64 +1,66 @@
-array = [int(x) for x in input("Введите числа от 1 до 999 в любом порядке, через пробел: ").split()]
+# На входе получаем последовательность чисел и сразу преобразовываем ее в список
+arr = [int(x) for x in input("Введите целые числа от 0 до 10000 через пробел: ").split()]
 
-def merge_sort(array):  # "разделяй"
-    if len(array) < 2:  # если кусок массива равен 2,
-        return array[:]  # выход из рекурсии
+# Для сортировки списка по возрастанию применим метод сортировки слиянием
+def merge_sort(arr):  
+    if len(arr) < 2:  # если в массиве меньше 2 элементов,
+        return arr[:]  # возвращаемся
     else:
-        middle = len(array) // 2  # ищем середину
-        left = merge_sort(array[:middle])  # рекурсивно делим левую часть
-        right = merge_sort(array[middle:])  # и правую
-        return merge(left, right)  # выполняем слияние
+        middle = len(arr) // 2  # иначе делим список пополам
+        left = merge_sort(arr[:middle])  # рекурсивно делим левую часть списка, реализуем срез
+        right = merge_sort(arr[middle:])  # рекурсивно делим правую часть списка
+        return merge(left, right)  
 
-
+# применим функцию для объединения двух списков
 def merge(left, right):
-    result = []
-    i, j = 0, 0
+    c = []  # задаем список
+    i, j = 0, 0 # определяем два указателя, которые будут указывать на первые элементы списков left и right соответственно
 
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
+    while i < len(left) and j < len(right): # пока i не превысит длину списка left и j не превысит длину списка right
+        if left[i] < right[j]: # проверяем, кто из элементов меньше
+            c.append(left[i]) # то в список с мещаем i элемент из списка left
+            i += 1 # и двигаем указатель i на единицу
         else:
-            result.append(right[j])
-            j += 1
+            c.append(right[j]) # иначе в список с помещаем j элемент из списка left
+            j += 1 # и двигаем указатель j на единицу
 
     while i < len(left):
-        result.append(left[i])
+        c.append(left[i])
         i += 1
 
     while j < len(right):
-        result.append(right[j])
+        c.append(right[j])
         j += 1
 
-    return result
+    return c
 
-print(merge_sort(array))
+print(merge_sort(arr))
 
 
-def binary_search(array, element, left, right):
+def binary_search(arr, num, left, right):
     if left > right:  # если левая граница превысила правую,
         return False  # значит элемент отсутствует
 
     middle = (right + left) // 2  # находимо середину
-    if array[middle] == element:  # если элемент в середине,
+    if arr[middle] == num:  # если элемент в середине,
         return middle  # возвращаем этот индекс
-    elif element < array[middle]:  # если элемент меньше элемента в середине
+    elif num < arr[middle]:  # если элемент меньше элемента в середине
         # рекурсивно ищем в левой половине
-        return binary_search(array, element, left, middle - 1)
+        return binary_search(arr, num, left, middle - 1)
     else:  # иначе в правой
-        return binary_search(array, element, middle + 1, right)
+        return binary_search(arr, num, middle + 1, right)
 
 
 while True:
     try:
-        element = int(input("Введите число от 1 до 999: "))
-        if element < 0 or element > 999:
+        num = int(input("Введите целое число от 1 до 9999: "))
+        if num < 0 or num >= 9999:
             raise Exception
         break
     except ValueError:
-        print("Нужно ввести число!")
+        print("Ошибка ввода. Введите целое число")
     except Exception:
-        print("Неправильный диапазон!")
+        print("Ошибка ввода. Введите число от 1 до 9999")
 
 
-print(binary_search(array, element, 0,  len(array)))
+print(binary_search(arr, num, 0,  len(arr)))
